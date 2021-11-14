d3fe27925f3ee06c26039e2102dd0fa7718b0fd4
matichek <mstrlekar@gmail.com>
2021-11-14 12:22:40 +0100

    Sitemap - traversing the API finished V0_1

--- pages/sitemap.vue
+++ pages/sitemap.vue
@@ -9,27 +9,55 @@
   <li v-for="(user, i) in usersArrayPrepared" :key="i">
     {{user.user_name}}
     <ul>
-      <li v-for="(album, j) in RelatedIdsUserAlbum(user['userId'])" :key="j">
-        {{album['album_title']}}
+      <li style="display: inline-block" v-for="(album, j) in RelatedIdsUserAlbum(user['userId'])" :key="j">
+        <strong>Album: {{album['album_title']}}</strong>
+
+        <ul style="list-style: none">
+          <li style="float: left;"  v-for="(photo, k) in RelatedPhotosAlbum(user['userId'])" :key="k">
+            <v-img max-width="100" :src="photo['thumb']"></v-img>
+          </li>
+        </ul>
       </li>
-
     </ul>
-
   </li>
 
 </ul>
 
 
+<!-- Testing snackbar - notice for api finishing -->
+
+<v-snackbar
+      v-model="snackbar"
+      :timeout="timeout"
+    >
+      {{ snackbarText }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="blue"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+    </template>
+</v-snackbar>
+
 
 <h1>Random number</h1>
 {{randomNumber}}
+
+<!--
+<h1>Photos prepared</h1>
+{{photosArrayPrepared}}
 
 <h1>Album prepared</h1>
 {{albumsArrayPrepared}}
 
 <h1>Users Prepared</h1>
 {{usersArrayPrepared}}
-
+-->
 
 
 </v-container>
@@ -44,31 +72,36 @@ export default {
 
   data() {
     return {
+
+      // Used arrays and vars
+
       usersArray: [],
       albumsArray: [],
-      albumsNames: [],
+      photosArray: [],
       albumsArrayPrepared: [],
       usersArrayPrepared: [],
+      photosArrayPrepared: [],
       randomNumber: null,
-
-
-
+      snackbar: false,
+      snackbarText: "Photo API loaded.",
+      timeout: 500
     }
   }, computed: {
 
       // Connect both userID from two different arrays
 
       RelatedIdsUserAlbum() {
-
         return id => this.albumsArrayPrepared.filter(album => album.userId === id)
+      },
 
+      RelatedPhotosAlbum() {
+        return id => this.photosArrayPrepared.filter(album => album.albumId === id)
       }
 
   },
 
-
   mounted() {
-    this.prepareAlbums()
+
   },
 
   // Async getting all the API calls
@@ -77,6 +110,7 @@ export default {
 
     await this.getUsers()
     await this.getAlbums()
+    await this.getPhotos()
 
 
   }, updated() {
@@ -105,7 +139,6 @@ export default {
             })
 
           })
-
     },
 
     // Second API call for Albums
@@ -136,27 +169,31 @@ export default {
       })
     },
 
-    // Get Photos
+    // Get Photos from API into array
 
     getPhotos() {
 
     return this.$axios
 
-      .get('https://jsonplaceholder.typicode.com/albums')
+      .get('https://jsonplaceholder.typicode.com/photos')
       .then((res) => {
 
         // Load all data into object array
-        this.albumsArray = res.data
+        this.photosArray = res.data
 
-        // Prepare object for merge with users object. And lets just use id and title.
-        this.albumsArrayPrepared = this.albumsArray.map(function(item) {
+        // Prepare object for merge with pictures object. Reduce size of array.
+        this.photosArrayPrepared = this.photosArray.map(function(item) {
           return {
-            userId: item.userId,
-            album_id: item.id,
-            album_title: item.title
+            albumId: item.albumId,
+            url: item.url,
+            thumb: item.thumbnailUrl
           }
 
+
+
         });
+
+        this.snackbar = true
 
       })
     }
